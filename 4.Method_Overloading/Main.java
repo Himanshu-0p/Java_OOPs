b77e2a0815dcdd2fda9180b946535f7d55f49cc3
Himanshu Patil <patilhimanshu1102@gmail.com>
2025-09-24 20:04:30 +0530

    ...

---
+++ 4.Method_Overloading/Main.java
@@ -0,0 +1,22 @@
+class Calculator{
+      public int add(int num1,int num2,int num3){
+            return num1+num2+num3;
+      }
+      public int add(int num1,int num2){
+            return num1+num2;
+      }
+      public double add(double num1,int num2){
+             return num1+num2;
+      }
+}
+
+
+
+public class Main {
+    public static void main(String [] agrs){
+       Calculator calci = new Calculator();
+       int r1 = calci.add(10,14,15);
+       System.out.println(r1);
+
+    } 
+}
