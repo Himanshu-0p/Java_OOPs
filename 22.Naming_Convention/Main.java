6fdb451bd35ca82964c662c1a1458d4eb9151fa3
Himanshu Patil <patilhimanshu1102@gmail.com>
2025-09-29 12:42:26 +0530

    ...

---
+++ 22.Naming_Convention/Main.java
@@ -0,0 +1,12 @@
+//Camel Casing
+
+//class and interface -  Calc , Runable
+//variable  and method - marks , show()
+//constants - PIE,BRAND
+
+//showMyMarks()
+//MyData
+
+// age - variable
+// DATA - constants
+// Human() - constructor
