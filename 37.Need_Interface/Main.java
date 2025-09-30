e74b3d55e343fa0783c3b0422852222a0561f3c5
Himanshu Patil <patilhimanshu1102@gmail.com>
2025-09-30 14:48:32 +0530

    ...

---
+++ 37.Need_Interface/Main.java
@@ -0,0 +1,39 @@
+interface Computer
+{
+	 void code();
+}
+
+class Laptop implements Computer
+{
+	public void code()
+	{
+		System.out.println("code, compile, run");
+	}
+}
+
+class Desktop implements Computer
+{
+	public void code()
+	{
+		System.out.println("code, compile, faster");
+	}
+}
+class Developer
+{
+	public void devApp(Computer lap)
+	{
+		lap.code();
+	}
+}
+
+public class Main {
+    public static void main(String[] args) {
+ 
+        Computer lap=new Laptop();
+        Computer desk=new Desktop();
+        
+        Developer navin=new Developer();
+        navin.devApp(lap);
+        
+    }
+}
